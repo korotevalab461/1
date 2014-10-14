8737e8eccd7edc71f48fafb072c4e4173f908827
Michail Koroteev <sairos@bk.ru>
2014-10-14 15:05:27 +0400

    create frame

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-﻿#-------------------------------------------------------------------------------
-# Name:        модуль1
-# Purpose:
-#
-# Author:      sejros
-#
-# Created:     14.10.2014
-# Copyright:   (c) sejros 2014
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-#!/usr/bin/env python
+﻿import simpleguitk as simplegui
+import random, math
 
-def main():
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400
+
+
+def draw():
     pass
 
-if __name__ == '__main__':
-    main()
+def keydown():
+    pass
+
+def keyup():
+    pass
+
+def restart():
+    pass
+
+
+# create frame
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+frame.add_button("Restart", restart, 100)
+
+# start frame
+frame.start()
