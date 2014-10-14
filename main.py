41a1d35db5958451751de9b7e975a0ecc2657658
Michail Koroteev <sairos@bk.ru>
2014-10-14 15:10:38 +0400

    add score drawing

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import random, math
 WIDTH = 600
 HEIGHT = 400
 
-
 def draw(c):
-    pass
+    global score1, score2
+
+    c.draw_text(str(score1), (185, 60), 40, "White")
+    c.draw_text(str(score2), (400, 60), 40, "White")
 
 def keydown(key):
     pass
@@ -16,7 +18,9 @@ def keyup(key):
     pass
 
 def restart():
-    pass
+    global score1, score2
+
+    score1, score2 = 0, 0
 
 
 # create frame
@@ -25,6 +29,8 @@ frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
 frame.set_keyup_handler(keyup)
 frame.add_button("Restart", restart, 100)
+
+restart()
 
 # start frame
 frame.start()
